9a29cf212a500a8c6f0d9b0cff6d4d35330e98e8
sataporn1995 <satapornchai2538@gmail.com>
2022-09-07 11:30:30 +0700

    First commit

--- src/components/page-wrapper/Footer.tsx
+++ src/components/page-wrapper/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer:React.FC = () => {
   return (
     <footer className="w-100 position-fixed bottom-0 text-center text-light">
-      Copyright &copy; 2022 By <b className="text-danger">PROGAMMER</b>
+      Copyright &copy; 2022 By <b className="text-info">Sataporn1995</b>
     </footer>
   );
 }
